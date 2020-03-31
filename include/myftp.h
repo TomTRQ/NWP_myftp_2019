54d3c5a1dde9e47d34159c25d2c7e312d063a4d0
Tom TRIQUET <triquet.tom@epitech.eu>
2020-03-31 15:28:25 +0200

    [MODIF] src/main.c, beginning Step 4 of the Bootstrap, replacing the fork by a select

--- include/myftp.h
+++ include/myftp.h
@@ -8,9 +8,17 @@
 #ifndef MYFTP_
 #define MYFTP_
 
+#define SOCKET_ERROR -1
+#define ERROR 84
+#define CORRECT 0
+
 #include <stdbool.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
 
 #endif /* !MYFTP_ */
--- src/main.c
+++ src/main.c
@@ -5,31 +5,83 @@
 ** myftp
 */
 
+#include <unistd.h>
+#include <signal.h>
+#include <arpa/inet.h>
 #include "myftp.h"
+
+//nc -C 127.0.0.1 4242
+
+bool is_wrong_parameters(int argc, char **argv)
+{
+    return (false);
+}
 
 bool print_help(int argc, char **argv)
 {
     if (argc == 2 && strcmp(argv[1], "-help") == 0) {
-        printf("USAGE: ./myftp port path");
+        printf("USAGE: ./myftp port path\n");
+        printf("\tport\tis the port number on which the server socket listens\n");
+        printf("\tpath\tis thr path to the home directory for the Anonymous user\n");
         return (true);
     } else if (argc == 2) {
         printf("Need 2 arguments as parameter\n");
-        exit(84);
+        exit(ERROR);
     }
     return (false);
 }
 
 int start_program(char **argv)
 {
-    return (0);
+    struct sockaddr_in sin;
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    socklen_t len_sin = sizeof(sin);
+
+    if (server_socket < 0)
+        return (SOCKET_ERROR);
+    sin.sin_addr.s_addr = htonl(INADDR_ANY);   
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons(atoi(argv[1]));
+    if (bind(server_socket, (struct sockaddr*)&sin, len_sin) < 0)
+        return (SOCKET_ERROR);
+    listen(server_socket, 10);
+    return (server_socket);
+}
+
+int wait_for_client(int server_socket, int port)
+{
+    int csock = 0;
+    struct sockaddr_in cin;
+    pid_t child_pid = 0;
+    socklen_t len_cin = sizeof(cin);
+
+    printf("En attente qu'un client se connecte sur le port %d...\n", port);
+    while (1) {
+        csock = accept(server_socket, (struct sockaddr*)&cin, &len_cin);
+        if (csock < 0)
+            return (SOCKET_ERROR);
+        child_pid = fork();
+        if (child_pid == 0) {
+            printf("Un client se connecte de %s:%d\n\n", inet_ntoa(cin.sin_addr), htons(cin.sin_port));
+            sleep(5);
+            write(csock, "Hello world!!\n", 16);
+            kill(getpid(), SIGKILL);
+        }
+        close(csock);
+    }
+    close(server_socket);
+    return (CORRECT);
 }
 
 int main(int argc, char **argv)
 {
-    if (argc == 1 || argc > 3)
-        return (84);
+    int server_socket = 0;
+
+    if (argc == 1 || argc > 3 || is_wrong_parameters(argc, argv))
+        return (ERROR);
     else if (print_help(argc, argv))
-        return (0);
-    else
-        return (start_program(argv));
+        return (CORRECT);
+    server_socket = start_program(argv);
+    if (server_socket == SOCKET_ERROR || wait_for_client(server_socket, atoi(argv[1])) == SOCKET_ERROR)
+        return (ERROR);
 }
