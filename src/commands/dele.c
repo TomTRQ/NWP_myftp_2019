e9ba6b2bd22b656277ad1fa61321a99a5a010d5a
Tom TRIQUET <triquet.tom@epitech.eu>
2020-04-15 12:00:24 +0200

    [MODIF] commands/dele.c, finished command

--- src/commands/dele.c
+++ src/commands/dele.c
@@ -6,10 +6,20 @@
 */
 
 #include "myftp.h"
+#include <fcntl.h>
 
-void dele(char *commands, client_t *client, server_t server)
+void dele(char *file, client_t *client, server_t server)
 {
     if (!client->is_connected)
         return (send_message("530 Need connection\r\n", client->socket));
-    send_message(command_array[5].message, client->socket);
+    if (!file)
+        return (send_message("501 DELE takes a file as \
+parameter\r\n", client->socket));
+    if (open(file, O_RDONLY) == -1)
+        return (send_message("550 file not found\r\n", client->socket));
+    if (remove(file) == 0)
+        send_message(command_array[5].message, client->socket);
+    else
+        send_message("500 An error occured with the DELE \
+command\r\n", client->socket);
 }
