3d884d3c8fd7cda5be161bca6f9bce50414755c5
Tom TRIQUET <triquet.tom@epitech.eu>
2020-04-16 11:08:39 +0200

    [MODIF] stor.c

--- src/commands/stor.c
+++ src/commands/stor.c
@@ -8,15 +8,24 @@
 #include "myftp.h"
 #include <fcntl.h>
 
-void stor(char *commands, client_t *client, server_t server)
+void send_file(char *file, client_t *client)
+{
+    int csock = 0;
+
+    send_message(command_array[12].message, client->socket);
+    send_message("226 Closing data connection\r\n", client->socket);
+}
+
+void stor(char *file, client_t *client, server_t server)
 {
     if (!client->is_connected)
         return (send_message("530 Need account for \
 storing files\r\n", client->socket));
-    if (!commands)
+    if (!file)
         return (send_message("501 STOR takes a pathname \
 as parameter\r\n", client->socket));
-    if (open(commands, O_RDONLY) == -1)
+    if (open(file, O_RDONLY) == -1)
         return (send_message("550 file not found\r\n", client->socket));
     send_message(command_array[12].message, client->socket);
+    send_file(file, client);
 }
