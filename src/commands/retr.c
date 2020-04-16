797cc53f9351092624279c7d78f095c9362ab496
Tom TRIQUET <triquet.tom@epitech.eu>
2020-04-16 22:01:48 +0200

    [MODIF] RETR command, now popen takes the command as parameter

--- src/commands/retr.c
+++ src/commands/retr.c
@@ -8,18 +8,35 @@
 #include "myftp.h"
 #include <fcntl.h>
 
+char *cat_command(char *file)
+{
+    char *command = malloc(sizeof(4 + strlen(file)));
+
+    if (!command)
+        return (NULL);
+    strcpy(command, "cat ");
+    strcat(command, file);
+    return (command);
+}
+
 void get_file(char *file, client_t *client)
 {
     FILE *fp;
     struct sockaddr_in cin;
     socklen_t len_cin = sizeof(cin);
     int csock = accept(client->data_socket, (struct sockaddr *)&cin, &len_cin);
+    char *command = cat_command(file);
 
-    send_message(command_array[13].message, client->socket);
-    fp = popen("cat main.c", "r");
-    message_loop(fp, csock);
-    send_message("226 Closing data connection\r\n", client->socket);
-    close(csock);
+    if (command) {
+        send_message(command_array[13].message, client->socket);
+        fp = popen(command, "r");
+        message_loop(fp, csock);
+        send_message("226 Closing data connection\r\n", client->socket);
+        close(csock);
+        free(command);
+    } else {
+        send_message("500 Error with the RETR command\r\n", client->socket);
+    }
 }
 
 void retr(char *file, client_t *client, server_t server)
@@ -32,6 +49,5 @@ files\r\n", client->socket));
 parameter\r\n", client->socket));
     if (open(file, O_RDONLY) == -1)
         return (send_message("550 file not found\r\n", client->socket));
-    send_message(command_array[11].message, client->socket);
     get_file(file, client);
 }
